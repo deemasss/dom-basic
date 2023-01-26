5d957b0e49cacd082f662dd2139f28e6ab4ce968
deemasss <dimon.limon.s90@gmail.com>
2023-01-26 08:16:44 +0200

    Add files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+    let setRedColor = document.querySelectorAll('li');
+    for (let i = 0; i < setRedColor.length; i++) {
+        if (i % 2 === 0) {
+            setRedColor[i].style.backgroundColor = 'red';
+        };
+    };
+};
 
-export const findElement = () => {};
+export const findElement = () => {
+    let elem = document.querySelector('li');
+    let elements = document.querySelectorAll('li');
+    for (let i = 0; i < elements.length; i++) {
+        if (elem.matches('.likedItem')) {
+            elem.style.backgroundColor = 'blue';
+        } else {
+            elem = elem.nextElementSibling;
+        };
+    };
+};
